480384d48c828dc4ac5a396a1938f8fb4444e7f4
agatha-duzan <agathaduzan@gmail.com>
2025-01-08 14:43:28 +0100

    consistency in exact model being used (redo all with gpt-4o-2024-11-20)

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ with open('/home/agatha/Desktop/MA3/sem proj/api_key_nlp_lab.txt', 'r') as file:
     OPENAI_API_KEY = file.read().strip()
 
 # Dataset
-DATASET_NAME ='agatha-duzan/advanced_algebra_af' # advanced_algebra_af OR number_theory_af'
+DATASET_NAME ='agatha-duzan/advanced_algebra_af' # BASE: number_theory_af ; ADVANCED: advanced_algebra_af
 FEW_SHOT_EXAMPLES_PATH = '/home/agatha/Desktop/MA3/sem proj/autoformalization_with_hypothesis/data/8_shot_proofnet_lean4_decomp.jsonl'
-# OLD: '/home/agatha/Desktop/MA3/sem proj/autoformalization_with_hypothesis/data/12_shot_proofnet_lean4.jsonl'
 
 # Model and provider
 DEFAULT_MODEL = 'gpt-4o-2024-11-20'
